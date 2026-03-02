4d132d1c8b9fc297d7fc65de7b086d920e27e01d
tolarom <tolarom27@gmail.com>
2026-03-03 06:47:10 +0700

    hello

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        scanner.close();
     }
 }
