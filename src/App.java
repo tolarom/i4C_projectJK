1f49c2e482dd560172ac3803a79c6dffd1b88f52
tolarom <tolarom27@gmail.com>
2026-03-03 06:43:18 +0700

    add input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.prinln("Hello, World!");
+        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
     }
 }
