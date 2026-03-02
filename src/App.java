ffa2424d30df2c7e12a024a9b254993df0bc2a1d
tolarom <tolarom27@gmail.com>
2026-03-03 06:47:29 +0700

    heeeel

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
     }
 }
